26b2115d2a440c07419aa43cae009f502f759938
web15 <charles@websiteciphers.com>
2018-04-04 16:44:35 -0400

    vars added

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clearTasks');
+const filter = document.querySelector('filter');
+const taskInput = document.querySelector('#task');
