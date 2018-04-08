7583f6f1735a81ccfc5cdaf489a248a7b9182d70
web15 <charles@websiteciphers.com>
2018-04-08 03:13:18 -0400

    b4 error

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+
 }
 
 // Add Task
@@ -20,11 +23,35 @@ function addTask(e)  {
     alert('Add a task');
   }
 
-  // Create li element
-  const li = document.createElement('li');
-  // Add class
-  li.className = 'collection-item';
+// Create li element
+const li  = document.createElement('li');
+// Add class
+li.className = 'collection-item';
+// Create text node and append to li
+li.appendChild(document.createTextNode(taskInput.value));
+// Creat new link element
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// Append the link to li
+li.appendChild(link);
 
+// Append li to ul
+taskList.appendChild(li);
+
+// Clear input
+taskInput.value = ' ';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure?')) {
+    e.target.parentElement.parentElement.remove();
+    }
+  }
 }
